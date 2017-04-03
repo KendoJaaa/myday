e9cd22d4afcff3f9a4833988c11eafb85070df97
Siwakorn Thitisaksakul <clever_buffalo@hotmail.com>
2017-04-03 09:51:42 +0700

    donet

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import './index.css'
 
-import App from './App'
-import React from 'react'
 import ReactDOM from 'react-dom'
 import firebase from 'firebase'
 import firebaseui from 'firebaseui'
 
+import App from './App'
+import './index.css'
+
 // Initialize Firebase
-var config = {
+const config = {
   apiKey: 'AIzaSyDB-3Hz6XGhYI_m3s7V9jzo-7TCeTA26_8',
   authDomain: 'myday-1e6f2.firebaseapp.com',
   databaseURL: 'https://myday-1e6f2.firebaseio.com',
@@ -15,31 +15,33 @@ var config = {
   storageBucket: 'myday-1e6f2.appspot.com',
   messagingSenderId: '432497065369'
 }
-firebase.initializeApp(config)
 
 // FirebaseUI config.
-var uiConfig = {
+const uiConfig = {
   signInSuccessUrl: 'www.google.co.th',
   signInOptions: [
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID
   ],
   // Terms of service url.
   tosUrl: '<your-tos-url>'
 }
 
+const ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+firebase.initializeApp(config)
+
 // Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth())
 // The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig)
 
-let user
-firebase.auth().onAuthStateChanged(function (response) {
-  user = response
-}, function (error) {
+firebase.auth().onAuthStateChanged((response) => {
+  const user = response
+  console.log('kendojaa', user)
+}, (error) => {
   console.log(error)
 })
 
 ReactDOM.render(
-  <App user={{displayName: 'kendo'}}/>,
+  <App user={{ displayName: 'kendo' }} />,
   document.getElementById('root')
-);
+)
